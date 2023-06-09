427f76e9798082ba49581b182ac3949148639d6d
SanaSanss <fouzia.asloum@outlook.fr>
2023-06-09 10:41:48 +0200

    php1

--- php/index.php
+++ php/index.php
@@ -1,68 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
-    <link rel="stylesheet" href="about-me.html">
-    <link rel="stylesheet" href="my-dreams.html">
-    <link rel="stylesheet" href="my-passions.html">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/about-me.css">
+    <link rel="stylesheet" href="css/my-dreams.css">
+    <link rel="stylesheet" href="css/my-passions.css">
     <link rel="stylesheet" href="#cv">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-              <!--HEDAER-->
-    <header>
-        <div class="content">
-            <h1>Asloum Fouzia</h1>
-            <h1>Asloum Fouzia</h1>
-        </div>
-        <div class="work"><p>Développeuse web et web mobile</p></div>
-        <div class="phrase"><p>-- Phrase d'accroche --</p></div>
-    </header>
 
-               <!--NAV-->
-    <nav>
-        <a href="index.html">Accueil</a>
-        <a href="about-me.html" target="_blank">A propos de moi</a>
-        <a href="my-dreams.html" target="_blank">Mes rêves d'apprenant</a>
-        <a href="my-passions.html" target="_blank">Mes passions</a>
-        <a href="#" target="_blank">Mon CV</a>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" 
-        rel="stylesheet">
-    </nav>
+<?php include './includes/header.html'; ?>
+
+<?php include './includes/nav.html'; ?>
+
+<?php
+if (isset ($_GET['sana'])){
+        $url = $_GET['sana'];
+       
+    if ($url === 'about-me.html') {
+      include './includes/about-me.html';
+    } else if ($url === 'my-dreams.html') {
+        include './includes/my-dreams.html';
+    } else if ($url === 'my-passions.html') {
+        include './includes/my-passions.html';
+    }
+      
+    
+}
+
+ 
+?>
+
+<?php include './includes/footer.html'; ?>
 
 
-              <!--SECTION-->
-
-    <section class="index">
-
-        <h1>Mon profil</h1>
-        <div class="pic">
-            <a href="images/hinata.jpg"><img src="images/hinata-mini.jpg" alt="pp-hinata"></a>
-        </div>
-
-        <div class="name">
-            <p>Asloum Fouzia</p>
-        </div>
-
-        <div class="intro">
-            <p>Passionée d'informatiques et de jeux vidéos depuis toute jeune mais ayant effectué un bac ES en
-                grandissant,
-                je me suis orientée vers une filière commerciale qui ne m'a finalement pas plue <br>
-                J'ai alors décidé après 4 ans d'arrêter ce cursus et de me réorienter vers ma passion réelle,
-                l'informatique
-            </p>
-            <p>Je suis actuellement en formation en vue d'obtenir un titre de développeuse web et web mobile sur le
-                court terme <br>
-                et envisage une poursuite d'études afin de devenir ingénieure en robotique sur du moyen terme
-            </p>
-        </div>
-    </section>
 
 </body>
 
